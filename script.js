2eca1445f3d9f81209b8138d057ff6bf304675f2
Kiri <kirilynne@gmail.com>
2021-10-01 16:22:23 -0400

    pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-var formEl = document.querySelector("")
+var formEl = document.querySelector(".form");
+var inputEl = document.querySelector(".input-search");
+var buttonEl = document.querySelector(".search-btn");
+var selectEl = document.querySelector(".options");
+
+//drop down has options that can be selected
+
+//user types in "input" and hits "search" -- search loads options from api
+
+//fetch data from external api
+//give parameters
+//selector for the input of the search
+//json to display fetched data
+
